3ce33ad88a90be8e7ab62eff2a68d75e546b6259
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 01:23:16 +0530

    Create product router 🛣️✅

---
+++ routes/productRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+} from "../controllers/ProductController.js";
+
+const productRouter = express.Router();
+
+productRouter.post("/", createProduct);
+productRouter.get("/", getProducts);
+
+export default productRouter;
