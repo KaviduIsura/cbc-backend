0504975ac8adce659201ce09e6932c9d6123e4d4
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 03:01:21 +0530

    add order router to index file 📝✅

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter.js";
 import jwt, { decode } from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config();
 
@@ -40,6 +41,7 @@ app.use((req, res, next) => {
 //Routes
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
