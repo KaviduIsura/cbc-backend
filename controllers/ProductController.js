1b3927d5b65dff3c5236789d8b2aafb52e651301
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 01:14:52 +0530

    create a function to save products to admin🧑‍💼✅

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,3 +1,28 @@
 import Product from "../models/Product.js";
+import { isAdmin } from "./UserController.js";
 
-export function createProduct(req, res) {}
+export function createProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.json({
+      message: "Please login as administrator to add products!",
+    });
+    return;
+  }
+
+  const newProductData = req.body;
+
+  const product = new Product(newProductData);
+
+  product
+    .save()
+    .then(() => {
+      res.json({
+        message: "Product Created",
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: error,
+      });
+    });
+}
