61ac434b8414a50a27b9e0a5c2c685e0fb21fd7d
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-16 11:03:11 +0530

    create reviewcontroller and create functions accodeing to requirements ✅🕹️

---
+++ controllers/ReviewController.js
@@ -0,0 +1,10 @@
+import reviewModel from "../models/Review";
+
+//can do customers
+export async function saveReviews(req, res) {}
+
+//can do all customers and admin
+export async function getReviews(req, res) {}
+
+//can do only admin (delete)
+export async function deleteReviwe(req, res) {}
--- models/Review.js
+++ models/Review.js
@@ -1,27 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
 const reviewSchema = mongoose.Schema({
-  name:{
-    type:String,
-    required:true,
+  name: {
+    type: String,
+    required: true,
   },
-  email:{
-    type:String,
-    required:true,
+  email: {
+    type: String,
+    required: true,
   },
-  review:{
-    type:String,
-    required:true,
+  review: {
+    type: String,
+    required: true,
   },
-  rating:{
-    type:number,
-    required:true
-  }
-  createdAt:{
-    type:Date,
-    require:true,
-    default:Date.now
-  }
-})
-const reviewModel = mongoose.Model("reviews",reviewSchema)
-export default reviewModel
+  rating: {
+    type: number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    require: true,
+    default: Date.now,
+  },
+});
+const reviewModel = mongoose.Model("reviews", reviewSchema);
+export default reviewModel;
